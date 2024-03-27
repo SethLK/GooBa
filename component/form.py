66ed51bca80429c2e19f6d25207a86e37bf8c916
SethLK <sethlk2006@gmail.com>
2024-03-27 21:13:27 +0630

    Input field done

--- component/form.py
+++ component/form.py
@@ -21,3 +21,31 @@ class Form:
         return form
 
 
+class InputField:
+    def __init__(self, name, **attributes):
+        self.name = name
+        self.type = "text"
+        self.label = None
+        self.attributes = attributes
+
+    def __str__(self):
+        input_ = ""  # Define input_ variable here
+        attribute_string = ' '.join([f'{key}="{value}"' for key, value in self.attributes.items()])
+
+        if 'className' in self.attributes:
+            attribute_string = attribute_string.replace('className', 'class')
+
+        if '\n' in self.name:
+            self.name = self.name.replace('\n', '')
+            input_ = f'<input type="{self.type}" name="{self.name}"><br>\n'
+
+        else:
+            input_ = f'<input type="{self.type}" name="{self.name}">\n'
+
+        if self.label:
+            input_ = str(self.label) + input_
+
+        elif attribute_string:
+            input_ = f'<input type="{self.type}" name="{self.name} {attribute_string}">'
+
+        return input_
