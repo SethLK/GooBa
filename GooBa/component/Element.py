a135b6543861312371884054f1443693df704659
SethLK <sethlk2006@gmail.com>
2024-08-11 18:39:41 +0630

    Remove parent and done inline style

--- GooBa/component/Element.py
+++ GooBa/component/Element.py
@@ -4,31 +4,44 @@ class Element:
         self.attributes = attributes
         self.text = None
         self.children = []
+        self.style = {}  # Use a dictionary for inline styles
 
     def appendChild(self, *children):
         self.children.extend(children)
 
+    def _format_style(self):
+        if not self.style:
+            return ''
+        style_items = []
+        for key, value in self.style.items():
+            # Ensure the value is a string
+            style_items.append(f'{key}: {value};')
+        return ' '.join(style_items)
+
     def __str__(self):
         attribute_string = ' '.join([f'{key}="{value}"' for key, value in self.attributes.items()])
+
+        # Add inline style if it exists
+        style_string = self._format_style()
+        if style_string:
+            attribute_string += f' style="{style_string}"'
 
         if 'className' in self.attributes:
             attribute_string = attribute_string.replace('className', 'class')
 
         if self.text is not None:
-            return f'<{self.tag} {attribute_string}>{self.text}</{self.tag}>\n'
+            content = f'{self.text}'
+        else:
+            content = ''.join(str(child) for child in self.children)
+
+        if self.text:
+            return f'<{self.tag} {attribute_string}>{content}</{self.tag}>\n'
         else:
             children_ = ''.join(str(child) for child in self.children)
             return f'<{self.tag} {attribute_string}>\n{children_}\n</{self.tag}>'
-
 
     def __add__(self, other):
         if isinstance(other, Element):
             return self.__str__() + other.__str__()
         else:
             return self.__str__() + str(other)
-
-
-class Link(Element):
-    def __init__(self, href, **attributes):
-        super().__init__('a', **attributes)
-        self.attributes['href'] = href
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from GooBa import Document, Element
 
+# Create a new document
 doc = Document()
-div = Element("div")
+
+# Create elements
+div = Element("div", id="main-div")
+div.style = {
+    "background-color": "red"
+}
 
 h1 = Element("h1")
 h1.text = "Hello ld"
 
+h1.style = {
+    "color": "#00ff00",
+}
+
 div.appendChild(h1)
 
-h1.text = "Hello There"
+h1_2 = Element("h1")
+h1_2.text = "Hello There"
 
-div.appendChild(h1)
+div.appendChild(h1_2)
 
+# Add elements to the document
 doc.body(div)
+
+# Build the document
 doc.build()
